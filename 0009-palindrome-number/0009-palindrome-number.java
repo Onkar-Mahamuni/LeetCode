20de1723af614e6f0ba496aba7c823418e9887ab
Onkar Mahamuni <121336300+Onkar-Mahamuni@users.noreply.github.com>
2024-05-13 20:06:18 +0530

    Time: 5 ms (74.97%), Space: 44.5 MB (6.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        
+        while(temp > 0){
+            rev = rev*10 + temp % 10; 
+            temp = temp/10;
+        }
+        
+        return x == rev;
+    }
+}
